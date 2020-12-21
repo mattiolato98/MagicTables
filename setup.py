0de3b1a30291d0e53252cf4860db993e4cd60ceb
Gabriele Mattioli <gabrymattioli@gmail.com>
2020-12-21 13:01:10 +0100

    Setup updated

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
     README = readme_file.read()
 
 setup_args = dict(
     name = 'django-magic-tables',
-    version = '0.1.1',
+    version = '0.3.1',
     description = 'A Django app to turn easily QuerySets into tables',
     long_description_content_type= 'text/markdown',
     long_description = README,
@@ -13,10 +13,11 @@ setup_args = dict(
     author = 'Gabriele Mattioli',
     author_email = 'gabrymattioli@gmail.com',
     license = 'MIT', 
+    packages=find_packages(),
     keywords= ['Table', 'Magictable'],
     download_url='https://pypi.org/project/django-magic-tables/0.1/',
 )
 
 if __name__ == "__main__":
-    setup(**setup_args)
+    setup(**setup_args, include_package_data=True)
 
